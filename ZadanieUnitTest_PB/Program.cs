046ba891f21584c8314b0ade40a3172394aae25f
Piotr Burzawa <storm.burzawa@gmail.com>
2022-01-17 13:33:08 +0100

    Dodano podstawowe wartości do obliczenia delty oraz stworzono oddzielny projekt dla testów jednostkowych -w tym klasę do kalkulacji pierwiastków kwadratowych.

--- ZadanieUnitTest_PB/Program.cs
+++ ZadanieUnitTest_PB/Program.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 namespace ZadanieUnitTest_PB
 {
     class Program
-    {
+    {   
+
         static void Main(string[] args)
         {
+            Console.Write("Podaj wartosc a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartosc b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartosc c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+// double delta = b * b - 4 * a * c;
+
         }
     }
 }
