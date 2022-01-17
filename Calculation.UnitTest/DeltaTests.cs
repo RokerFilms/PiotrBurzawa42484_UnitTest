0d152735d2e3c0347229befb371a496d477184f7
Piotr Burzawa <storm.burzawa@gmail.com>
2022-01-17 15:26:49 +0100

    Przeprowadzenie pierwszego testu w ramach testu działania odwołań klas

--- Calculation.UnitTest/DeltaTests.cs
+++ Calculation.UnitTest/DeltaTests.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
+using ZadanieUnitTest_PB;
 
 namespace Calculation.UnitTest
 {
     class DeltaTests
     {
-//update
+        [Test]
+        public void CheckDelta()
+        {
+            double x1, x2, x0;
+
+            double a = 4;
+            double b = 5;
+            double c = 3;
+
+            //arrange
+            var cm = new CalculatorMain();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+            //assert
+            Assert.AreEqual(2, delta);
+        }
     }
 }
---
+++ ZadanieUnitTest_PB/CalculatorMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieUnitTest_PB
+{
+        public class CalculatorMain
+        {
+            public double Delta(double a, double b, double c)
+            {
+                return b * b - 4 * a * c;
+            }
+        }
+}
