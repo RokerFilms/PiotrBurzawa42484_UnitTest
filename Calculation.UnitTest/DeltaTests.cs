9086d0284caf5acda07b5fa9277ef9df5d94b07b
Piotr Burzawa <storm.burzawa@gmail.com>
2022-01-17 18:11:06 +0100

    Wprowadzenie innych możliwości dla Assert (Assert.AreEqual, Assert.IsTrue).

--- Calculation.UnitTest/DeltaTests.cs
+++ Calculation.UnitTest/DeltaTests.cs
@@ -8,8 +8,13 @@ namespace Calculation.UnitTest
 {
     class DeltaTests
     {
+
+        // TEST dla mozliwosci Assert.That
+
+
+
         [Test]
-        public void Delta_WhenReturnDwa()
+        public void Delta_String_WhenReturnDwa()
         {
             double x1, x2, x0;
 
@@ -24,12 +29,12 @@ namespace Calculation.UnitTest
             var delta = cm.Delta(a, b, c);
 
 
-            //assert oraz metoda
+            //assert 
 
             Assert.That(delta, Is.EqualTo("Dwa pierwiastki rzeczywiste"));
         }
         [Test]
-        public void Delta_WhenReturnJeden()
+        public void Delta_String_WhenReturnJeden()
         {
             double x1, x2, x0;
 
@@ -44,7 +49,7 @@ namespace Calculation.UnitTest
             var delta = cm.Delta(a, b, c);
 
 
-            //assert oraz metoda
+            //assert 
 
             Assert.That(delta, Is.EqualTo("Jeden pierwiastek rzeczywisty"));
 
@@ -52,7 +57,7 @@ namespace Calculation.UnitTest
 
 
         [Test]
-        public void Delta_WhenReturnBrak()
+        public void Delta_String_WhenReturnBrak()
         {
             double x1, x2, x0;
 
@@ -67,12 +72,167 @@ namespace Calculation.UnitTest
             var delta = cm.Delta(a, b, c);
 
 
-            //assert oraz metoda
+            //assert 
 
             Assert.That(delta, Is.EqualTo("Brak pierwiastkow rzeczywistych"));
 
         }
 
 
+
+        //Testy dla mozliwosci Assert.Equal
+
+        [Test]
+        public void Delta_Int_WhenReturnBrak()
+        {
+            double x1, x2, x0;
+
+            double a = 1;
+            double b = -2;
+            double c = 1;
+
+            //arrange
+            var cm = new CalculatorInt();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+
+            //assert
+
+            Assert.AreEqual(1, delta);
+
+        }
+
+        [Test]
+        public void Delta_Int_WhenReturnJeden()
+        {
+            double x1, x2, x0;
+
+            double a = 2;
+            double b = 1;
+            double c = 2;
+
+            //arrange
+            var cm = new CalculatorInt();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+
+            //assert
+
+            Assert.AreEqual(0, delta);
+
+        }
+
+
+        [Test]
+        public void Delta_Int_WhenReturnDwa()
+        {
+            double x1, x2, x0;
+
+            double a = 6;
+            double b = 10;
+            double c = -1;
+
+            //arrange
+            var cm = new CalculatorInt();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+
+            //assert
+
+            Assert.AreEqual(2, delta);
+
+        }
+
+
+// TEST dla mozliwosci Assert.IsTrue
+
+
+        [Test]
+        public void Delta_Bool_WhenReturnDwa()
+        {
+            double x1, x2, x0;
+
+            double a = 6;
+            double b = 10;
+            double c = -1;
+
+            //arrange
+            var cm = new CalculatorBooleanForDwa();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+
+            //assert
+
+            Assert.IsTrue(delta);
+
+        }
+
+        [Test]
+        public void Delta_Bool_WhenReturnJeden()
+        {
+            double x1, x2, x0;
+
+            double a = 6;
+            double b = 10;
+            double c = -1;
+
+            //arrange
+            var cm = new CalculatorBooleanForDwa.CalculatorBooleanForJeden();
+
+            //act
+            var delta = cm.Delta(a, b, c);
+
+
+            //assert
+
+            Assert.IsTrue(delta);
+
+        }
+
+    
+
+
+    [Test]
+    public void Delta_Bool_WhenReturnBrak()
+    {
+        double x1, x2, x0;
+
+        double a = 3;
+        double b = 3;
+        double c = 3;
+
+        //arrange
+        var cm = new CalculatorBooleanForDwa.CalculatorBooleanForBrak();
+
+        //act
+        var delta = cm.Delta(a, b, c);
+
+
+        //assert
+
+        Assert.IsTrue(delta);
+
     }
+
 }
+
+
+
+
+
+
+}
+
+
+
+
+
+
--- ZadanieUnitTest_PB/CalculatorMain.cs
+++ ZadanieUnitTest_PB/CalculatorMain.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ZadanieUnitTest_PB
 {
-        public class CalculatorMain
+
+
+    //Ponizej klasa przeznaczona dla wyjscia Assert.That dla string
+
+
+    public class CalculatorMain
         {
         public double x1, x2, x0;
             public string Delta(double a, double b, double c)
@@ -38,4 +43,120 @@ namespace ZadanieUnitTest_PB
 
         }
         }
-}
+
+
+
+
+
+    //Ponizej klasa przeznaczona dla wyjscia Assert.AreEqual z uzyciem int
+
+
+
+
+    public class CalculatorInt
+    {
+        public double x1, x2, x0;
+        public int Delta(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                delta = (Math.Sqrt(delta));
+                x1 = (-b - delta) / (2 * a);
+                x2 = (-b + delta) / (2 * a);
+                Console.WriteLine("Sa dwa pierwiastki: " + x1 + " oraz " + x2);
+                return 2;
+            }
+            else if (delta == 0)
+            {
+                delta = (Math.Sqrt(delta));
+                x0 = -b / (2 * a);
+                Console.WriteLine("Jeden pierwiastek rzeczywisty: " + x0);
+                return 1;
+            }
+            else
+                Console.WriteLine("Brak pierwiastkow");
+            return 0;
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+    //Ponizej klasy przeznaczone dla wyjscia Assert.IsTrue dla boolean
+
+
+
+
+
+
+    public class CalculatorBooleanForDwa
+    {
+        public double x1, x2, x0;
+        public bool Delta(double a, double b, double c)
+        {
+            double delta = b * b - 4 * a * c;
+            if (delta > 0)
+            {
+                delta = (Math.Sqrt(delta));
+                x1 = (-b - delta) / (2 * a);
+                x2 = (-b + delta) / (2 * a);
+                Console.WriteLine("Sa dwa pierwiastki: " + x1 + " oraz " + x2);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+
+
+        public class CalculatorBooleanForJeden
+        {
+            public double x1, x2, x0;
+            public bool Delta(double a, double b, double c)
+            {
+                double delta = b * b - 4 * a * c;
+                if (delta > 0)
+                {
+                    delta = (Math.Sqrt(delta));
+                    x0 = -b / (2 * a);
+                    Console.WriteLine("Jeden pierwiastek rzeczywisty: " + x0);
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+        }
+
+
+
+        public class CalculatorBooleanForBrak
+        {
+            public double x1, x2, x0;
+            public bool Delta(double a, double b, double c)
+            {
+                double delta = b * b - 4 * a * c;
+                if (delta > 0 || delta == 0)
+                {
+                    return false;
+                }
+                else
+                    Console.WriteLine("Brak pierwiastkow rzeczywistych");
+                    return true;
+
+            }
+        }
+
+
+
+
+    }
+    }
